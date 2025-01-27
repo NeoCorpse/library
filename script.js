e41017483b83aa0478e99bb3d4d30e507d23faee
Ian <neocorpse@proton.me>
2025-01-27 13:15:54 +0300

    Style the header section
    Included small animation for the search input that phases in and out
    while adding a placeholder based on the state of the input

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+// Adds and removes placeholder on search input depending on its state
+const search = document.querySelector("input[type='search']");
+search.addEventListener("mouseover", setPlaceholder);
+search.addEventListener("focus", setPlaceholder);
+search.addEventListener("mouseout", () => {
+  if (search.hasAttribute("placeholder") && document.activeElement !== search) {
+    removePlaceholder();
+  }
+});
+search.addEventListener("focusout", removePlaceholder);
+
+function setPlaceholder() {
+  search.setAttribute("placeholder", "Search for your favourite book");
+}
+
+function removePlaceholder() {
+  search.removeAttribute("placeholder");
+}
